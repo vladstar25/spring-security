ed30bb098d1c189a6fa74adfff9bf86cecb107b0
Админко <vladstar25@yandex.ru>
2020-08-10 19:31:03 +0700

    2.4.2 Исправил. Перенес все конфиги в config.

---
+++ src/main/java/web/config/SpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package web.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
+
+    //обязателен для не boot-приложения. Кода в нем нет, но требуется для регистрации секьюрити в Спринг-контейнере.
+}
--- src/main/java/web/security/SpringSecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package web.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
-
-    //обязателен для не boot-приложения. Кода в нем нет, но требуется для регистрации секьюрити в Спринг-контейнере.
-}
